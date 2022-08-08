e76eff10704c00d5f33f4c6a5547c729e6b895ca
“Brigg-commit” <ibridget95@gmail.com>
2022-08-08 13:37:15 +0100

    commit printf.c file

---
+++ printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * _printf - function to print my printf of "c" "s" and "i".
+ * @format: getting the my printf argument
+ * Return the specifier if found true
+ */
+int _print(const char *format, ...)
+{
+  int i = 0;
+  int flagg = 0;
+  va_list args;
+  va_start(args, format);
+  while (format[i])
+    {
+      while (format[i] == "%")
+	{
+	  switch (format[i + 1])
+	    {
+	    case "c";
+	    flagg += print_char(args)
+	      i += 2;
+	    break;
+	    }
+	  i++;
+	}
+      if (format[i])
+	{
+	  _putchar(format[i]);
+	  flagg++
+        }
+      i++;
+    }
+  va_ends(args);
+  return (flagg);
+}
