57af00262bd05f4fe1320385066769ffbb2a4917
Oluwatomi Olagunju <90867477+ooluwatom@users.noreply.github.com>
2022-08-09 21:59:38 +0100

    Editing _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,18 +1,12 @@
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and error is there returned
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
-<<<<<<< HEAD
-return (write(1, &c, 1));
-=======
-    return (write(1, &c, 1));
->>>>>>> 19b99087a4bbab7dd24904725b2ee84f711e0240
+	return (write(1, &c, -1));
 }
