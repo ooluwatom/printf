2b97684f2babb45387d051fc1ad96b66d3409dc0
“Brigg-commit” <ibridget95@gmail.com>
2022-08-09 15:58:23 +0100

    commit _putchar.c again

--- _putchar.c
+++ _putchar.c
@@ -5,14 +5,9 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and error is there returned
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
-<<<<<<< HEAD
-return (write(1, &c, 1));
-=======
-    return (write(1, &c, 1));
->>>>>>> 19b99087a4bbab7dd24904725b2ee84f711e0240
+	return (write(1, &c, 1));
 }
